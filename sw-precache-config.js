4f12b6f436d6acc6c76daae819034728a8179c1a
taitt <taitt@vng.com.vn>
2017-02-03 21:14:46 +0700

    update story

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -15,7 +15,8 @@ module.exports = {
         '/index.html',
         '/manifest.json',
         '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-        '/src/css/**/*'
+        '/src/css/**/*',
+        '/src/story/**/*'
     ],
     navigateFallback: 'index.html'
 };
